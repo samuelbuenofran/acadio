879e60c9465bfea233d3bbb7936facc814bf46fd
Samuel Bueno Francisco <samuel.buenofran@gmail.com>
2024-06-12 22:37:11 -0300

    Added compiled documentation

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"deprecated"},{"l":"gui"},{"l":"org.eclipse.wb.swing"},{"l":"professor"},{"l":"prototype"},{"l":"services"}];updateSearchResults();
